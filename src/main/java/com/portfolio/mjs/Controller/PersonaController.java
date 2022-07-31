794fd23770debe77849dafa77f391023cdc6bc17
MarianoSaid <marianoj_said@hotmail.com>
2022-07-31 03:02:33 -0300

    incorporación de app front en el back

--- src/main/java/com/portfolio/mjs/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mjs/Controller/PersonaController.java
@@ -16,9 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
-
-//@CrossOrigin(origins = {"http://localhost:4200", "https://miportfolio-dsalcedo.web.app"})
+@CrossOrigin(origins = "https://appfrontms.web.app")
 
 public class PersonaController { 
     @Autowired IPersonaService ipersonaService;
