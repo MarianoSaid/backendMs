ab088f38cfe757967a879a50ffff043093f88694
MarianoSaid <marianoj_said@hotmail.com>
2022-07-31 03:34:30 -0300

    agregado en la url de / prueba

--- src/main/java/com/portfolio/mjs/Controller/ProyectosController.java
+++ src/main/java/com/portfolio/mjs/Controller/ProyectosController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://appfrontms.web.app")
+@CrossOrigin(origins = "https://appfrontms.web.app/")
 public class ProyectosController {
     @Autowired IProyectosService iproyectosService;
     
